d4e840dac1a5e7bb5ec7210aad3719172fa7f420
uzair330 <uzair330@gmail.com>
2023-07-11 00:18:15 +0500

    Catategory update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Newsletter from "@/components/Newsletter";
 import SwipperProduct from "@/components/SwipperProduct";
 import Unique2 from "@/components/Unique02";
 import { Hero } from "@/view/Hero";
-import { ProductList } from "@/view/ProductList";
+
 import Promo02 from "@/view/Promo02";
 import Promotion from "@/view/Promotion";
 import Image from "next/image";
